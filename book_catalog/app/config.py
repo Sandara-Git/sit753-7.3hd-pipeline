10276a717bb83821d41ab60144ccbfb86d15bdf0
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-09-05 22:24:52 +1000

    Update config.py

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://book_catalog_n60i_user:8prUeK5Mdv7yg1bCAPb165m36uefjjmq@dpg-crclg63v2p9s73ci46hg-a.oregon-postgres.render.com/book_catalog_n60i')
