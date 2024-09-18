8fc3146fc69ee388cc0be8ffe1aadcb432ac5086
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-09-18 12:38:03 +1000

    Update database.py

--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://sit772_part3_user:8RkWhS2lk6zAjGBYyLwTOp1PBbpGGCge@dpg-crl3aid6l47c73fqrq60-a.oregon-postgres.render.com/sit772_part3" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL =os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
