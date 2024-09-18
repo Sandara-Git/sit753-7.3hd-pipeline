6da56dc47369b560c4ecad37a2406253b015af99
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-09-18 12:36:54 +1000

    Update config.py

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit772_part3_user:8RkWhS2lk6zAjGBYyLwTOp1PBbpGGCge@dpg-crl3aid6l47c73fqrq60-a.oregon-postgres.render.com/sit772_part3')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
