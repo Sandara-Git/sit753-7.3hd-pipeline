196980b70f321e95a4c59f9310fd712f02f3a757
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-09-18 12:13:30 +1000

    Update config.py

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://book_catalog_n60i_user:8prUeK5Mdv7yg1bCAPb165m36uefjjmq@dpg-crclg63v2p9s73ci46hg-a.oregon-postgres.render.com/book_catalog_n60i')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit772_part3_user:8RkWhS2lk6zAjGBYyLwTOp1PBbpGGCge@dpg-crl3aid6l47c73fqrq60-a.oregon-postgres.render.com/sit772_part3')
