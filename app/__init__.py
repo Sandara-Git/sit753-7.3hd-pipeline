5f9acff02b3ded051a8dc40c75bb118f4f01de51
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2025-10-02 12:58:05 +1000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 def home():
     return jsonify(ok=True, msg="Hello from CI/CD")
 
+@app.get("/")
+def index():
+    return "OK", 200
+
 @app.get("/health")
 def health():
-    return jsonify(status="pass")
+    return jsonify(status="ok"), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
